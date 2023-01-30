f10d2493fb6be6d8445197d49292c04fd74a0a44
Cristian Talaigua <cnhoyos@gmail.com>
2023-01-30 14:48:15 -0500

    feat: modified files for recied port

--- src/app.js
+++ src/app.js
@@ -9,14 +9,15 @@ import tipoDocumentoRouter from "./routes/tipo.documento.routes"
 
 const app=express();
 
-//Settings
-app.set("port",4000);
-
 //Middelware
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
+  
+//Settings
+app.set( "PORT",process.env.PORT || 4000)
+
 
 //Routes 
 app.use("/api/languages",languagesRoutes),
